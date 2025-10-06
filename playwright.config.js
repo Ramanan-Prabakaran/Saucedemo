1af026461f5e04b3eaacfbf4cdc0a056624e93de
Ramanan-Prabakaran <ramananprabakaran97@gmail.com>
2025-10-06 06:40:00 +0530

    Updated Playwright config for video recording and HTML report

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,9 @@
 export default {
   reporter: [["html", { outputFolder: "html-report", open: "never" }]],
-  use: { headless: true, screenshot: "on", video: "retain-on-failure" },
+  use: {
+    headless: false, // Show browser for demo recording
+    screenshot: "on",
+    video: "on", // Record every test (not just failures)
+    trace: "retain-on-failure", // Optional - helps debug or showcase
+  },
 };
